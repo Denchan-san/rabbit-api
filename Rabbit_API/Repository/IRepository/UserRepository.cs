be9bbf21ff83f2e9e087c4f952ab431a1d04db11
Jokir <doner24072004@gmail.com>
2024-04-21 22:57:17 +0300

    Controller and repository of user needs to be implimented

---
+++ Rabbit_API/Repository/IRepository/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using Rabbit_API.Data;
+using Rabbit_API.Models.Dto;
+using Rabbit_API.Models.Dto.Users;
+
+namespace Rabbit_API.Repository.IRepository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public bool isUniqueUser(string username)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(x => x.UserName == username);
+            if (user == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDTO> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
